94b716289992ea415001e3eb93190450a2110b18
debacodex <debashissabar0@gmail.com>
2025-07-20 16:01:34 +0530

    update

---
+++ app/src/main/java/com/magic/in/ApiClient.java
@@ -0,0 +1,36 @@
+package com.magic.in;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class ApiClient {
+	private static ApiClient instance;
+	private RequestQueue requestQueue;
+	private static Context ctx;
+	
+	private ApiClient(Context context) {
+		ctx = context;
+		requestQueue = getRequestQueue();
+	}
+	
+	public static synchronized ApiClient getInstance(Context context) {
+		if (instance == null) {
+			instance = new ApiClient(context);
+		}
+		return instance;
+	}
+	
+	public RequestQueue getRequestQueue() {
+		if (requestQueue == null) {
+			requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+		}
+		return requestQueue;
+	}
+	
+	public <T> void addToRequestQueue(Request<T> req) {
+		getRequestQueue().add(req);
+	}
+}
---
+++ app/src/main/java/com/magic/in/SearchableFragment.java
@@ -0,0 +1,5 @@
+package com.magic.in;
+
+public interface SearchableFragment {
+	void onSearch(String query);
+}
